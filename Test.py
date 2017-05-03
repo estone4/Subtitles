a789bba2ed90ef445ad95e64b7656d63f422c96c
Eric Stone <estone4@gmail.com>
2017-05-03 08:55:48 -0500

    Will now read UTF-8 files.  Unsure what I changed to allow this.

--- Test.py
+++ Test.py
@@ -4,6 +4,7 @@ import platform
 import codecs
 import tkinter as tk
 from tkinter import filedialog
+#from collections import Counter
 #print(os.name)
 print(platform.system())
 #if platform.system()=='Windows':
@@ -13,13 +14,27 @@ root.withdraw()
 file_path = filedialog.askopenfilename()
 
 def file_read(fname):
-    txt = open(fname,encoding="UTF-8")
-    file=txt.read()
-    count = file.count('w')
+    with open(fname, 'r') as txt:
+        #root = tk.Tk()
+        #root.withdraw()
+        #file_path = filedialog.askopenfilename()
+
+    #txt = open(fname,encoding="UTF-8")
+        file=txt.read()
+    count = file.count('ý')
+    #with open(fname, 'a') as txt:
+
+        #txt.write("This is a test")
     print(file)
     print(count)
     #file=txt.read()
     #print(file.find('w'))
 
+
+
 if file_path != "":
     file_read(file_path)
+#elif file_path == "":
+#    root = tk.Tk()
+#    root.withdraw()
+#    file_path = filedialog.askopenfilename()
